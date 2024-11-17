b1c4c6d70c13efd8a4b399fbcdd604510036897a
Gustavo Rabelo <gurabelo09@gmail.com>
2024-11-17 11:55:07 -0300

    add pergunta repository

---
+++ src/main/java/com/semando/ltda/gateways/repositories/PerguntaRepository.java
@@ -0,0 +1,11 @@
+package com.semando.ltda.gateways.repositories;
+
+import com.semando.ltda.domains.Pergunta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {
+    // Método customizado para buscar perguntas por nível (idLevel)
+    List<Pergunta> findByLevelId(Long levelId);
+}
