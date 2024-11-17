daad4b493c25b37dd6a8e14370de24bdbf71fa6f
Gustavo Rabelo <gurabelo09@gmail.com>
2024-11-17 01:32:35 -0300

    create project

---
+++ src/main/java/com/semando/ltda/SpringSemeandoApplication.java
@@ -0,0 +1,13 @@
+package com.semando.ltda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSemeandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSemeandoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semando/ltda/SpringSemeandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semando.ltda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSemeandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
